dde48b17b60d11f6eb2dde3b7e1fe94e94689ec0
sipahisnm <75900861+sipahisnm@users.noreply.github.com>
2021-04-14 10:01:34 -0400

    Update app.py

--- aws-work-area/project.2/roman-project/app.py
+++ aws-work-area/project.2/roman-project/app.py
@@ -34,4 +34,4 @@ def total():
                 
 
 if __name__=='__main__':
-        app.run(debug=True)                                
+        app.run(host='0.0.0.0',port=80)                               
